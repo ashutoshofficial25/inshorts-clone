ca3911431b8b90b7140d593c8cc662e5cbc7e1ce
Ashutosh Maurya <ashutoshm9695@gmail.com>
2022-10-31 11:52:04 +0530

    humburger drawer

--- src/components/HumburgerDrawer.js
+++ src/components/HumburgerDrawer.js
@@ -1,24 +1,18 @@
 import React, { useState } from "react";
-import clsx from "clsx";
-import { Button, Divider, List, ListItem, ListItemText } from "@mui/material";
-import makeStyles from "@mui/material/makeStyles";
+import {
+  Box,
+  Button,
+  Divider,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+} from "@mui/material";
 import { SwipeableDrawer } from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import categories from "../data/category";
 
-const useStyles = makeStyles({
-  list: {
-    width: 200,
-    padddingLeft: 10,
-    paddingRight: 5,
-  },
-  fullList: {
-    width: "auto",
-  },
-});
-
 export default function SwipeableTemporaryDrawer({ setCategory }) {
-  const classes = useStyles();
   const [state, setState] = useState({ left: false });
 
   const toggleDrawer = (anchor, open) => (event) => {
@@ -33,46 +27,46 @@ export default function SwipeableTemporaryDrawer({ setCategory }) {
   };
 
   const list = (anchor) => (
-    <div
-      className={clsx(classes.list, {
-        [classes.fullList]: anchor === "top" || anchor === "bottom",
-      })}
+    <Box
+      sx={{ width: anchor === "top" || anchor === "bottom" ? "auto" : 250 }}
       role="presentation"
       onClick={toggleDrawer(anchor, false)}
       onKeyDown={toggleDrawer(anchor, false)}
     >
       <List>
-        <ListItem>Categories</ListItem>
+        <ListItem>
+          <ListItemButton>Categories</ListItemButton>
+        </ListItem>
+        <Divider />
       </List>
-      <Divider />
       <List>
         {categories.map((text) => (
-          <ListItem
-            style={{ height: 40, borderRadius: 3 }}
-            button
-            onClick={() => setCategory(text)}
-            key={text}
-          >
-            <ListItemText primary={text}></ListItemText>
+          <ListItem key={text} disablePadding>
+            <ListItem>
+              <ListItemButton>
+                <ListItemText>{text}</ListItemText>
+              </ListItemButton>
+            </ListItem>
           </ListItem>
         ))}
       </List>
-    </div>
+    </Box>
   );
 
   return (
     <div>
       <Button onClick={toggleDrawer("left", true)}>
         <MenuIcon />
-        <SwipeableDrawer
-          anchor={"left"}
-          open={state["left"]}
-          onClose={toggleDrawer("left", false)}
-          onOpen={toggleDrawer("left", true)}
-        >
-          {list("left")}
-        </SwipeableDrawer>
+        <p>Hello</p>
       </Button>
+      <SwipeableDrawer
+        anchor={"left"}
+        open={state["left"]}
+        onClose={toggleDrawer("left", false)}
+        onOpen={toggleDrawer("left", true)}
+      >
+        {list("left")}
+      </SwipeableDrawer>
     </div>
   );
 }
--- src/components/NavInshorts.js
+++ src/components/NavInshorts.js
@@ -1,19 +1,25 @@
 import React from "react";
 import "./NavInshorts.css";
 import HumburgerDrawer from "./HumburgerDrawer";
+import { Button } from "@mui/material";
+import { Container } from "@mui/system";
 
 const NavInshorts = ({ setCategory }) => {
   return (
     <div className="nav">
       <div className="menu">
-        <HumburgerDrawer setCategory={setCategory} />
+        <HumburgerDrawer
+          className="humburgerDrawer"
+          setCategory={setCategory}
+        />
 
         <img
           src="../logo144.png"
           style={{ cursor: "pointer" }}
           alt="inshorts-logo"
-          height="80%"
+          height="50rem"
         />
+        <Button>Login/signup</Button>
       </div>
     </div>
   );
